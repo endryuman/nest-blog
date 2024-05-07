d72ba7ea8ed02b0cd231e2f03e7670cd13c4b436
endryuman <132812966+endryuman@users.noreply.github.com>
2024-05-07 11:01:47 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
